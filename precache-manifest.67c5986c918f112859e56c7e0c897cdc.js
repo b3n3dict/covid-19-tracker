e7e2781a3809362f20acd00aa1306eca0fc84ac8
georgejeswin <jeswinmyladoor@gmail.com>
2020-09-20 12:44:59 +0530

    Updates

---
+++ precache-manifest.67c5986c918f112859e56c7e0c897cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b2221f5495e29ff960b5a0c0de9ecd",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "5f416dbc858e78f65df4",
+    "url": "/covid-19-tracker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "89618329a5139f02cbcb",
+    "url": "/covid-19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f416dbc858e78f65df4",
+    "url": "/covid-19-tracker/static/js/2.ad69122e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19-tracker/static/js/2.ad69122e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89618329a5139f02cbcb",
+    "url": "/covid-19-tracker/static/js/main.1022ed6d.chunk.js"
+  },
+  {
+    "revision": "023bf2af146bb9310a43",
+    "url": "/covid-19-tracker/static/js/runtime-main.ee0ec021.js"
+  }
+]);
